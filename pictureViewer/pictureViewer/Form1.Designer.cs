fe6e25464ad9f60cd358e889257dd8c69ca83389
than-ne <23520018@gm.uit.edu.vn>
2024-09-24 18:07:58 +0700

    Thai An, The Thinh, Hoang Chien cung thuc hien

---
+++ pictureViewer/pictureViewer/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace pictureViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonOpenDirectory = new System.Windows.Forms.Button();
+            this.textBoxDicrectory = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBoxImagePreview = new System.Windows.Forms.PictureBox();
+            this.listBoxImages = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImagePreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Directtory";
+            // 
+            // buttonOpenDirectory
+            // 
+            this.buttonOpenDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOpenDirectory.Location = new System.Drawing.Point(633, 19);
+            this.buttonOpenDirectory.Name = "buttonOpenDirectory";
+            this.buttonOpenDirectory.Size = new System.Drawing.Size(144, 40);
+            this.buttonOpenDirectory.TabIndex = 1;
+            this.buttonOpenDirectory.Text = "Open Directory";
+            this.buttonOpenDirectory.UseVisualStyleBackColor = true;
+            this.buttonOpenDirectory.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxDicrectory
+            // 
+            this.textBoxDicrectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDicrectory.Location = new System.Drawing.Point(115, 28);
+            this.textBoxDicrectory.Multiline = true;
+            this.textBoxDicrectory.Name = "textBoxDicrectory";
+            this.textBoxDicrectory.Size = new System.Drawing.Size(512, 24);
+            this.textBoxDicrectory.TabIndex = 2;
+            this.textBoxDicrectory.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.listBoxImages);
+            this.groupBox1.Location = new System.Drawing.Point(12, 74);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(235, 445);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Image";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.pictureBoxImagePreview);
+            this.groupBox2.Location = new System.Drawing.Point(253, 74);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(524, 445);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Image Preview";
+            // 
+            // pictureBoxImagePreview
+            // 
+            this.pictureBoxImagePreview.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxImagePreview.Location = new System.Drawing.Point(7, 22);
+            this.pictureBoxImagePreview.Name = "pictureBoxImagePreview";
+            this.pictureBoxImagePreview.Size = new System.Drawing.Size(511, 417);
+            this.pictureBoxImagePreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxImagePreview.TabIndex = 0;
+            this.pictureBoxImagePreview.TabStop = false;
+            // 
+            // listBoxImages
+            // 
+            this.listBoxImages.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBoxImages.FormattingEnabled = true;
+            this.listBoxImages.ItemHeight = 16;
+            this.listBoxImages.Location = new System.Drawing.Point(6, 18);
+            this.listBoxImages.Name = "listBoxImages";
+            this.listBoxImages.Size = new System.Drawing.Size(223, 420);
+            this.listBoxImages.TabIndex = 0;
+            this.listBoxImages.SelectedIndexChanged += new System.EventHandler(this.listBoxImages_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 531);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBoxDicrectory);
+            this.Controls.Add(this.buttonOpenDirectory);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImagePreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonOpenDirectory;
+        private System.Windows.Forms.TextBox textBoxDicrectory;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBoxImagePreview;
+        private System.Windows.Forms.ListBox listBoxImages;
+    }
+}
+
---
+++ pictureViewer/pictureViewer/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace pictureViewer
+{
+    public partial class Form1 : Form
+    {
+
+        string currentDir = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var fb = new FolderBrowserDialog();
+                if(fb.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    currentDir = fb.SelectedPath; // user chon file anh
+                    //Hien thi duong dan 
+                    textBoxDicrectory.Text = currentDir;
+                    //lay toan bo anh tu Directory
+                    var dirInfo = new DirectoryInfo(currentDir); //tao duong dan den directory
+                                                                 // lay files anh 
+                    var files = dirInfo.GetFiles().Where(c => (c.Extension.Equals(".jpg") || c.Extension.Equals(".png") || c.Extension.Equals(".jpeg") || c.Extension.Equals(".bmp")));
+                   foreach(var image in files)
+                    {
+                        // add anh vao listbox
+                        listBoxImages.Items.Add(image.Name);
+                    }
+
+                }
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("Không thể mở: " + ex.Message + " " + ex.Source);
+            }
+        }
+
+        private void listBoxImages_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                var selectedImage = listBoxImages.SelectedItems[0].ToString();
+                if ( !string.IsNullOrEmpty( selectedImage ) && !string.IsNullOrEmpty(currentDir))
+                {
+                    var fullPath = Path.Combine( currentDir, selectedImage );
+                    pictureBoxImagePreview.Image = Image.FromFile( fullPath );
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+
+}
