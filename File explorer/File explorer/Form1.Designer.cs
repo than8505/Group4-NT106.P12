bbccdf50cb8bbdaeeeb93cca2b52a5a6034ff4bd
HOANGCHIENlaptop <23520181@gm.uit.edu.vn>
2024-10-09 12:29:14 +0700

    Add files via upload
    File explorer

---
+++ File explorer/File explorer/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace File_explorer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnLeft = new System.Windows.Forms.Button();
+			this.btnRight = new System.Windows.Forms.Button();
+			this.lblPath = new System.Windows.Forms.Label();
+			this.btnOpen = new System.Windows.Forms.Button();
+			this.txtPath = new System.Windows.Forms.TextBox();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.SuspendLayout();
+			// 
+			// btnLeft
+			// 
+			this.btnLeft.Location = new System.Drawing.Point(12, 21);
+			this.btnLeft.Name = "btnLeft";
+			this.btnLeft.Size = new System.Drawing.Size(37, 23);
+			this.btnLeft.TabIndex = 0;
+			this.btnLeft.Text = "<<";
+			this.btnLeft.UseVisualStyleBackColor = true;
+			this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+			// 
+			// btnRight
+			// 
+			this.btnRight.Location = new System.Drawing.Point(55, 21);
+			this.btnRight.Name = "btnRight";
+			this.btnRight.Size = new System.Drawing.Size(37, 23);
+			this.btnRight.TabIndex = 1;
+			this.btnRight.Text = ">>";
+			this.btnRight.UseVisualStyleBackColor = true;
+			this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+			// 
+			// lblPath
+			// 
+			this.lblPath.AutoSize = true;
+			this.lblPath.Location = new System.Drawing.Point(110, 24);
+			this.lblPath.Name = "lblPath";
+			this.lblPath.Size = new System.Drawing.Size(37, 16);
+			this.lblPath.TabIndex = 2;
+			this.lblPath.Text = "Path:";
+			// 
+			// btnOpen
+			// 
+			this.btnOpen.Location = new System.Drawing.Point(692, 21);
+			this.btnOpen.Name = "btnOpen";
+			this.btnOpen.Size = new System.Drawing.Size(85, 23);
+			this.btnOpen.TabIndex = 4;
+			this.btnOpen.Text = "Open";
+			this.btnOpen.UseVisualStyleBackColor = true;
+			this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+			// 
+			// txtPath
+			// 
+			this.txtPath.Location = new System.Drawing.Point(153, 21);
+			this.txtPath.Name = "txtPath";
+			this.txtPath.Size = new System.Drawing.Size(533, 22);
+			this.txtPath.TabIndex = 5;
+			this.txtPath.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// listView1
+			// 
+			this.listView1.HideSelection = false;
+			this.listView1.Location = new System.Drawing.Point(12, 60);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(765, 378);
+			this.listView1.TabIndex = 6;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.txtPath);
+			this.Controls.Add(this.btnOpen);
+			this.Controls.Add(this.lblPath);
+			this.Controls.Add(this.btnRight);
+			this.Controls.Add(this.btnLeft);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnLeft;
+		private System.Windows.Forms.Button btnRight;
+		private System.Windows.Forms.Label lblPath;
+		private System.Windows.Forms.Button btnOpen;
+		private System.Windows.Forms.TextBox txtPath;
+		private System.Windows.Forms.ListView listView1;
+	}
+}
+
---
+++ File explorer/File explorer/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace File_explorer
+{
+	public partial class Form1 : Form
+	{
+		private ImageList imageList = new ImageList();
+		private List<string> navigationHistory = new List<string>();
+		private int currentHistoryIndex = -1;
+		public Form1()
+		{
+			InitializeComponent();
+			SetupListView();
+			SetupImageList();
+			InitializeContextMenu();
+			LoadFilesAndDirectories("D:\\"); // Load thư mục gốc D: khi khởi động
+		}
+		// Thiết lập ImageList và thêm các icon mặc định
+		private void SetupImageList()
+		{
+			imageList.ImageSize = new Size(16, 16);
+			imageList.Images.Add("folder", SystemIcons.WinLogo.ToBitmap());
+			imageList.Images.Add("file", SystemIcons.WinLogo.ToBitmap());
+			listView1.SmallImageList = imageList;
+		}
+		// Thiết lập ListView với các cột
+		private void SetupListView()
+		{
+			listView1.View = View.Details;
+			listView1.FullRowSelect = true;
+			listView1.Columns.Add("Name", 200);
+			listView1.Columns.Add("Date modified", 150);
+			listView1.Columns.Add("Type", 100);
+			listView1.Columns.Add("Size", 100);
+
+			listView1.MouseDoubleClick += listView1_MouseDoubleClick;
+			listView1.MouseClick += listView1_MouseClick;
+		}
+		// Tải các thư mục và tập tin vào ListView	
+
+		private void LoadFilesAndDirectories(string path)
+		{
+			listView1.Items.Clear();
+			DirectoryInfo dirInfo = new DirectoryInfo(path);
+
+			listView1.Items.Clear();
+
+			try
+			{
+				foreach (var dir in dirInfo.GetDirectories())
+				{
+					ListViewItem item = new ListViewItem(dir.Name);
+					item.ImageKey = "folder";
+					item.SubItems.Add(dir.LastWriteTime.ToString());
+					item.SubItems.Add("File folder");
+					item.SubItems.Add("");
+					item.Tag = dir.FullName;
+					listView1.Items.Add(item);
+				}
+
+				foreach (var file in dirInfo.GetFiles())
+				{
+					ListViewItem item = new ListViewItem(file.Name);
+					item.ImageKey = "file";
+					item.SubItems.Add(file.LastWriteTime.ToString());
+					item.SubItems.Add(file.Extension);
+					item.SubItems.Add((file.Length / 1024).ToString() + " KB");
+					item.Tag = file.FullName;
+					listView1.Items.Add(item);
+				}
+
+				// Cập nhật lịch sử nếu đang duyệt thư mục mới
+				if (currentHistoryIndex == -1 || navigationHistory[currentHistoryIndex] != path)
+				{
+					if (currentHistoryIndex < navigationHistory.Count - 1)
+					{
+						navigationHistory.RemoveRange(currentHistoryIndex + 1, navigationHistory.Count - currentHistoryIndex - 1);
+					}
+					navigationHistory.Add(path);
+					currentHistoryIndex++;
+				}
+
+				txtPath.Text = path;
+			}
+			catch (UnauthorizedAccessException)
+			{
+				MessageBox.Show("Access to the path is denied.");
+			}
+		}
+
+		private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			if (listView1.SelectedItems.Count > 0)
+			{
+				ListViewItem item = listView1.SelectedItems[0];
+				string selectedPath = item.Tag.ToString();
+
+				if (Directory.Exists(selectedPath))
+				{
+					LoadFilesAndDirectories(selectedPath);
+				}
+			}
+		}
+		private void listView1_MouseClick(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Right && listView1.FocusedItem != null && listView1.FocusedItem.Bounds.Contains(e.Location))
+			{
+				ContextMenuStrip contextMenu = new ContextMenuStrip();
+				contextMenu.Items.Add("Open", null, (s, ev) =>
+				{
+					if (Directory.Exists(listView1.FocusedItem.Tag.ToString()))
+					{
+						LoadFilesAndDirectories(listView1.FocusedItem.Tag.ToString());
+					}
+				});
+				contextMenu.Items.Add("Delete", null, (s, ev) =>
+				{
+					var selectedPath = listView1.FocusedItem.Tag.ToString();
+					if (Directory.Exists(selectedPath))
+					{
+						Directory.Delete(selectedPath);
+					}
+					else if (File.Exists(selectedPath))
+					{
+						File.Delete(selectedPath);
+					}
+					LoadFilesAndDirectories(Path.GetDirectoryName(selectedPath));
+				});
+				contextMenu.Show(listView1, e.Location);
+			}
+		}
+
+		private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnLeft_Click(object sender, EventArgs e)
+		{
+			if (currentHistoryIndex > 0)
+			{
+				currentHistoryIndex--;
+				string previousPath = navigationHistory[currentHistoryIndex];
+				LoadFilesAndDirectories(previousPath);
+			}
+		}
+
+		private void btnRight_Click(object sender, EventArgs e)
+		{
+			if (currentHistoryIndex < navigationHistory.Count - 1)
+			{
+				currentHistoryIndex++;
+				string nextPath = navigationHistory[currentHistoryIndex];
+				LoadFilesAndDirectories(nextPath);
+			}
+		}
+
+		private void btnOpen_Click(object sender, EventArgs e)
+		{
+			string path = txtPath.Text;
+			if (Directory.Exists(path))
+			{
+				LoadFilesAndDirectories(path);
+			}
+			else
+			{
+				MessageBox.Show("The path does not exist.");
+			}
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+		private string clipboardPath = null;
+		private bool isCutOperation = false;
+
+		private void InitializeContextMenu()
+		{
+			ContextMenuStrip contextMenu = new ContextMenuStrip();
+
+			// Tạo các mục menu
+			ToolStripMenuItem copyItem = new ToolStripMenuItem("Copy");
+			copyItem.Click += CopyItem_Click;
+			contextMenu.Items.Add(copyItem);
+
+			ToolStripMenuItem cutItem = new ToolStripMenuItem("Cut");
+			cutItem.Click += CutItem_Click;
+			contextMenu.Items.Add(cutItem);
+
+			ToolStripMenuItem pasteItem = new ToolStripMenuItem("Paste");
+			pasteItem.Click += PasteItem_Click;
+			contextMenu.Items.Add(pasteItem);
+
+			ToolStripMenuItem deleteItem = new ToolStripMenuItem("Delete");
+			deleteItem.Click += DeleteItem_Click;
+			contextMenu.Items.Add(deleteItem);
+
+			ToolStripMenuItem newFolderItem = new ToolStripMenuItem("New Folder");
+			newFolderItem.Click += NewFolderItem_Click;
+			contextMenu.Items.Add(newFolderItem);
+
+			listView1.ContextMenuStrip = contextMenu;
+		}
+
+		private void CopyItem_Click(object sender, EventArgs e)
+		{
+			if (listView1.SelectedItems.Count > 0)
+			{
+				clipboardPath = listView1.SelectedItems[0].Tag.ToString();
+				isCutOperation = false;
+			}
+		}
+
+		private void CutItem_Click(object sender, EventArgs e)
+		{
+			if (listView1.SelectedItems.Count > 0)
+			{
+				clipboardPath = listView1.SelectedItems[0].Tag.ToString();
+				isCutOperation = true;
+			}
+		}
+
+		private void PasteItem_Click(object sender, EventArgs e)
+		{
+			if (!string.IsNullOrEmpty(clipboardPath))
+			{
+				string destinationPath = Path.Combine(txtPath.Text, Path.GetFileName(clipboardPath));
+
+				try
+				{
+					if (File.Exists(clipboardPath))
+					{
+						if (isCutOperation)
+						{
+							File.Move(clipboardPath, destinationPath);
+						}
+						else
+						{
+							File.Copy(clipboardPath, destinationPath);
+						}
+					}
+					else if (Directory.Exists(clipboardPath))
+					{
+						if (isCutOperation)
+						{
+							Directory.Move(clipboardPath, destinationPath);
+						}
+						else
+						{
+							CopyDirectory(clipboardPath, destinationPath);
+						}
+					}
+					LoadFilesAndDirectories(txtPath.Text);
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show("An error occurred: " + ex.Message);
+				}
+				finally
+				{
+					clipboardPath = null;
+					isCutOperation = false;
+				}
+			}
+		}
+
+		private void DeleteItem_Click(object sender, EventArgs e)
+		{
+			if (listView1.SelectedItems.Count > 0)
+			{
+				string selectedPath = listView1.SelectedItems[0].Tag.ToString();
+				try
+				{
+					if (File.Exists(selectedPath))
+					{
+						File.Delete(selectedPath);
+					}
+					else if (Directory.Exists(selectedPath))
+					{
+						Directory.Delete(selectedPath, true);
+					}
+					LoadFilesAndDirectories(txtPath.Text);
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show("An error occurred: " + ex.Message);
+				}
+			}
+		}
+
+		private void NewFolderItem_Click(object sender, EventArgs e)
+		{
+			string newFolderPath = Path.Combine(txtPath.Text, "New Folder");
+			int count = 1;
+			while (Directory.Exists(newFolderPath))
+			{
+				newFolderPath = Path.Combine(txtPath.Text, $"New Folder ({count++})");
+			}
+
+			try
+			{
+				Directory.CreateDirectory(newFolderPath);
+				LoadFilesAndDirectories(txtPath.Text);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("An error occurred: " + ex.Message);
+			}
+		}
+
+		// Helper method to copy directories
+		private void CopyDirectory(string sourceDir, string destinationDir)
+		{
+			Directory.CreateDirectory(destinationDir);
+			foreach (var file in Directory.GetFiles(sourceDir))
+			{
+				string destFile = Path.Combine(destinationDir, Path.GetFileName(file));
+				File.Copy(file, destFile);
+			}
+
+			foreach (var dir in Directory.GetDirectories(sourceDir))
+			{
+				string destDir = Path.Combine(destinationDir, Path.GetFileName(dir));
+				CopyDirectory(dir, destDir);
+			}
+		}
+	}
+}
